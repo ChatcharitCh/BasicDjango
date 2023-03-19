d36e0c4b50eefbe2bed2696553afd0633dd423d4
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-03-19 20:44:58 +0700

    Created Field

--- myapp/models.py
+++ myapp/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # ไฟล์นี้จะให้เราออกแบบว่าข้อมูลที่จะจัดเก็บในฐานข้อมูลมีโครงสร้างเป็นอย่างไร
 class Person(models.Model): # ต้องสืบทอดมาจากคลาสโมเดล
-    pass
+    
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    age = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
