5d0da1e87d94deccb6a51723c0916e59cf0356f3
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-03-21 14:12:21 +0700

    View Data

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse # ส่งข้อความตอบกลับ
+from myapp.models import Person
 
 # Create your views here.
 def index(request): # สร้างฟังก์ชั่นส่งข้อความไปแสดงผลหน้าเว็ป
-    fname = "Chatcharit"
-    lname = "Choocherd"
-    age = 27
-    return render(request, "index.html", {"fname": fname, "lname": lname,"age": age}) # แทรกตัวแปรในหน้าเว็บ
+    all_person = Person.objects.all() # ดึงข้อมูลประชากรทั้งหมดมาทำงานที่ views.py
+    return render(request, "index.html", {"all_person": all_person}) # แทรกตัวแปรในหน้าเว็บ
 
 def about(request):
     return render(request, "about.html")
