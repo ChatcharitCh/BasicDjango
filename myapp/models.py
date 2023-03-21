b79ad274af7ab173eacfbf3b57acf38286220ab9
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-03-21 13:51:32 +0700

    Created Field

--- myapp/models.py
+++ myapp/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model): # สร้างคลาสข้อมูลประชากร
-    pass
+    
+    name = models.CharField(max_length = 100)
+    age = models.IntegerField()
+    date = models.DateField(auto_now_add = True)
